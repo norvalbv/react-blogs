e23e27ddb464959c7d63a0c7204100560451b105
norvalbv <benjinorval@gmail.com>
2023-09-17 14:01:28 +0100

    add support for delcartion files, vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react({ include: /\.(ts|tsx)$/ }), viteTsconfigPaths()],
+  plugins: [react({ include: /\.(ts|tsx)$/ }), viteTsconfigPaths(), dts({ rollupTypes: true })],
   assetsInclude: ['**/*.md'],
   build: {
-    lib: { entry: './src/index.tsx', name: 'react-blogs', fileName: 'react-blogs' },
+    lib: { entry: './src/index.tsx', name: 'index' },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react'],
       output: {
         globals: {
           react: 'react',
-          'react-dom': 'react-dom',
         },
       },
     },
