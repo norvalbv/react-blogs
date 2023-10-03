f65f358f4e96659fac868d26068b389bd40f197a
norvalbv <benjinorval@gmail.com>
2023-10-03 17:32:10 +0100

    add ts-node

--- src/utils/processLinks/__tests__/processLinks.test.ts
+++ src/utils/processLinks/__tests__/processLinks.test.ts
@@ -4,18 +4,16 @@ import { Blogs } from 'types';
 
 const blogs: Blogs[] = [
   {
-    id: 'memory-ram',
+    id: 'memory',
     file: 'memory',
-    title: { text: 'How Memory (RAM) Works' },
-    subtitle: { text: 'A deep insight to what RAM is and how it interacts with the computer.' },
+    title: { text: 'What is RAM?' },
     url: 'how-memory-works',
   },
   {
-    id: 'memory-heap',
-    file: 'memory-heap',
-    title: { text: 'What is the Memory Heap?' },
-    subtitle: { text: 'A deep insight to what RAM is and how it interacts with the computer.' },
-    url: 'memory-heap',
+    id: 'CPU',
+    file: 'cpu',
+    title: { text: 'What is the CPU?' },
+    url: 'what-is-the-cpu',
   },
 ];
 
@@ -27,10 +25,10 @@ const blogs: Blogs[] = [
  *
  * Inputs / Outputs:
  *
- * [[CPU]] -> [CPU](/blogs/CPU)
- * [[CPU#CPU1]] -> [CPU](/blogs/CPU#CPU1)
- * [[CPU|CPU2]] -> [CPU2](/blogs/CPU)
- * [[CPU#CPU1|CPU2]] -> [CPU2](/blogs/CPU#CPU1)
+ * [[CPU]] -> [CPU](?paramKey=CPU)
+ * [[CPU#CPU1]] -> [CPU](?paramKey=CPU#CPU1)
+ * [[CPU|CPU2]] -> [CPU2](?paramKey=CPU)
+ * [[CPU#CPU1|CPU2]] -> [CPU2](?paramKey=CPU#CPU1)
  * [[memory]] -> memory
  * [[memory#memory1]] -> memory
  * [[memory|memory2]] -> memory2
@@ -39,38 +37,48 @@ const blogs: Blogs[] = [
 
 describe('processLink', () => {
   test('should handle simple blog link', () => {
-    expect(processLink('[[Memory (RAM)]]')).toBe('[Memory (RAM)](/blog/how-memory-works)');
-  });
-
-  test('should handle blog link with anchor', () => {
-    expect(processLink('[[Memory (RAM)#memory]]')).toBe(
-      '[Memory (RAM)](/blog/how-memory-works#memory)'
+    expect(processLink({ allBlogs: blogs, blog: '[[Memory (RAM)]]', paramKey: 'blog' })).toBe(
+      '[Memory (RAM)](/blog/how-memory-works)'
     );
   });
 
+  test('should handle blog link with anchor', () => {
+    expect(
+      processLink({ paramKey: 'blog', allBlogs: blogs, blog: '[[Memory (RAM)#memory]]' })
+    ).toBe('[Memory (RAM)](/blog/how-memory-works#memory)');
+  });
+
   test('should handle blog link with name override', () => {
-    expect(processLink('[[Memory (RAM)|CPU2]]')).toBe('[CPU2](/blog/how-memory-works)');
+    expect(processLink({ paramKey: 'blog', allBlogs: blogs, blog: '[[Memory (RAM)|CPU2]]' })).toBe(
+      '[CPU2](/blog/how-memory-works)'
+    );
   });
 
   test('should handle blog link with both anchor and name override', () => {
     expect(
-      processLink('[[Memory (RAM)#Hierarchical Nature of Storage|CPU cache (L1, L2, L3 cache)]]')
+      processLink({
+        paramKey: 'blog',
+        allBlogs: blogs,
+        blog: '[[Memory (RAM)#Hierarchical Nature of Storage|CPU cache (L1, L2, L3 cache)]]',
+      })
     ).toBe('[CPU cache (L1, L2, L3 cache)](/blog/how-memory-works#Hierarchical Nature of Storage)');
   });
 
   test('should handle non-blog link', () => {
-    expect(processLink('[[CPU]]')).toBe('CPU');
+    expect(processLink({ paramKey: 'blog', allBlogs: blogs, blog: '[[CPU]]' })).toBe('CPU');
   });
 
   test('should handle non-blog link with anchor', () => {
-    expect(processLink('[[CPU#CPU1]]')).toBe('CPU');
+    expect(processLink({ paramKey: 'blog', allBlogs: blogs, blog: '[[CPU#CPU1]]' })).toBe('CPU');
   });
 
   test('should handle non-blog link with name override', () => {
-    expect(processLink('[[CPU|CPU2]]')).toBe('CPU2');
+    expect(processLink({ paramKey: 'blog', allBlogs: blogs, blog: '[[CPU|CPU2]]' })).toBe('CPU2');
   });
 
   test('should handle non-blog link with both anchor and name override', () => {
-    expect(processLink('[[CPU#CPU1|CPU2]]')).toBe('CPU2');
+    expect(processLink({ paramKey: 'blog', allBlogs: blogs, blog: '[[CPU#CPU1|CPU2]]' })).toBe(
+      'CPU2'
+    );
   });
 });
