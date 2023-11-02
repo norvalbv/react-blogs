a33da22ee627c5f503420d0ccebde83f1459f43c
norvalbv <benjinorval@gmail.com>
2023-11-02 20:30:23 +0000

    remove vite-plugin-markdown

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import { plugin as mdPlugin, Mode } from 'vite-plugin-markdown';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
@@ -10,12 +9,10 @@ export default defineConfig({
   plugins: [
     cssInjectedByJsPlugin(),
     vanillaExtractPlugin(),
-    mdPlugin({ mode: [Mode.HTML] }),
     react({ include: /\.(ts|tsx)$/ }),
     viteTsconfigPaths(),
     dts({ rollupTypes: true }),
   ],
-  assetsInclude: [/\.md$/, '**/*.md', '.md'],
   build: {
     lib: { entry: './src/index.ts', name: 'react-blogs', fileName: 'index' },
     rollupOptions: {
