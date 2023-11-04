d6e57ab8fdfde69bb8376adc5a43c3d50d1e47e1
norvalbv <benjinorval@gmail.com>
2023-11-04 21:56:07 +0000

    fix badge

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,12 +1,16 @@
+import useStore from 'hooks/useStore';
 import { ReactElement } from 'react';
-import { styles } from 'styles/themes.css';
+import { styles, themes } from 'styles/themes.css';
 
 type Props = {
   tag: string;
 };
 
 const Badge = ({ tag }: Props): ReactElement => {
-  return <div className={styles.badge}>{tag}</div>;
+  const theme = useStore((state) => state.theme);
+  return (
+    <div className={`${themes[theme?.theme || 'PLAIN_DARK'].nodes} ${styles.badge}`}>{tag}</div>
+  );
 };
 
 export default Badge;
