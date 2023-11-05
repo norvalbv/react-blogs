2aaf78621d1e388e075071840253714eb08265ce
norvalbv <benjinorval@gmail.com>
2023-11-05 14:59:52 +0000

    remove unused local storage setters

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,21 +1,3 @@
-import { DefTheme } from 'types';
-
-export const storeTheme = (theme: DefTheme): void =>
-  localStorage.setItem('react-blogs-theme', JSON.stringify(theme));
-
-export const getTheme = (): DefTheme => {
-  const theme = localStorage.getItem('react-blogs-theme');
-
-  if (theme) {
-    return JSON.parse(theme) as DefTheme;
-  }
-
-  const defaultTheme: DefTheme = {
-    theme: 'PLAIN_DARK',
-  };
-  return defaultTheme;
-};
-
 export const isLabelInProps = (props: object): object => {
   return 'label' in props ? { ...props, label: undefined } : { ...props };
 };
